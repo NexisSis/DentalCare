2fb803062abf0a9377478bd9121e92bb5801cee6
Nexis Sis <nexis.sis@gmail.com>
2018-06-18 23:08:44 +0300

    workayu

--- src/js/partials/animationSettings.js
+++ src/js/partials/animationSettings.js
@@ -13,3 +13,21 @@ $(document).on('click', 'a[href^="#"]', function (event) {
     }, 900);
 
 });
+
+// menu hover
+$(function() {
+    $('#menu,#top-nav ul').mouseenter(function() {
+        $('#header .hover-menu-bg').css('opacity', '1');
+        $('#header ul li ul').css('display','inherit');
+        $('#header ul li ul').css('opacity', '1');
+        $('#header ul li a').css('color', 'black');
+        $('#header ul li ul li a').css('color','#757575');
+
+    });
+    $('#header .hover-menu-bg').mouseleave(function () {
+        $('#header .hover-menu-bg').css('opacity', '0');
+        $('#header ul li ul').css('display','none');
+        $('#header ul li ul').css('opacity', '0');
+        $('#header ul li a').css('color', 'white');
+    });
+});
